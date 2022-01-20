861cef6f0ab0f6bea8b1f9f7bc01259bf8d640ec
Or nizri <54673058+OrNizri@users.noreply.github.com>
2022-01-20 20:47:41 +0200

    Add files via upload

---
+++ WebApplication4/ConfigSettings.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPiCurrencies
+{
+    public static class ConfigSettings
+    {
+        public static string connection { get; }
+
+        static ConfigSettings()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.Development.json");
+            configurationBuilder.AddJsonFile(path, false);
+            connection = configurationBuilder.Build().GetSection("ConnectionStrings:CurrenciesDatabase").Value;
+        }
+    }
+}
